bf45cf75917dfbfa7fde65bce202d8f776420ac6
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2024-01-16 22:55:53 +0900

    初期画面配置導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:simple_todo/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    initialLocation: "/",
+    routes: <RouteBase>[
+      GoRoute(
+        path: "/",
+        pageBuilder: (context, state) {
+          return const MaterialPage(
+            child: HomeScreen(),
+          );
+        },
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp.router(
+      theme: ThemeData(
+        colorSchemeSeed: Colors.pink,
+        useMaterial3: true,
       ),
+      routerConfig: _router,
+      builder: (context, child) {
+        return child!;
+      },
     );
   }
 }
