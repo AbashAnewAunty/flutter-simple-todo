0c80d667fafc770beb2427857e9fca49050dc51f
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2024-01-17 01:23:18 +0900

    ファイル分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:simple_todo/view/agreement_dart.dart';
-import 'package:simple_todo/view/code_input_page.dart';
-import 'package:simple_todo/view/home_screen.dart';
-import 'package:simple_todo/view/login_page.dart';
-import 'package:simple_todo/view/privacy_policy_page.dart';
-import 'package:simple_todo/view/profile_input_page.dart';
-import 'package:simple_todo/view/signup_complete_page.dart';
-import 'package:simple_todo/view/signup_page.dart';
-import 'package:simple_todo/view/turms_page.dart';
+import 'package:simple_todo/view/router.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  MainApp({super.key});
-
-  final GoRouter _router = GoRouter(
-    initialLocation: "/",
-    routes: <RouteBase>[
-      GoRoute(
-        path: "/",
-        pageBuilder: (context, state) {
-          return const MaterialPage(
-            child: HomeScreen(),
-          );
-        },
-        routes: <RouteBase>[
-          GoRoute(
-            path: "login",
-            pageBuilder: (context, state) {
-              return const MaterialPage(
-                child: LoginPage(),
-              );
-            },
-          ),
-          GoRoute(
-            path: "agreement",
-            pageBuilder: (context, state) {
-              return const MaterialPage(
-                child: AgreementPage(),
-              );
-            },
-            routes: <RouteBase>[
-              GoRoute(
-                path: "turms",
-                pageBuilder: (context, state) {
-                  return const MaterialPage(
-                    child: TurmsPage(),
-                  );
-                },
-              ),
-              GoRoute(
-                path: "privacy-policy",
-                pageBuilder: (context, state) {
-                  return const MaterialPage(
-                    child: PrivacyPolicyPage(),
-                  );
-                },
-              ),
-            ],
-          ),
-          GoRoute(
-            path: "signup",
-            pageBuilder: (context, state) {
-              return const MaterialPage(
-                child: SignUpPage(),
-              );
-            },
-            routes: <GoRoute>[
-              GoRoute(
-                path: "input-profile",
-                pageBuilder: (context, state) {
-                  return const MaterialPage(
-                    child: ProfileInputPage(),
-                  );
-                },
-              ),
-              GoRoute(
-                path: "input-code",
-                pageBuilder: (context, state) {
-                  return const MaterialPage(
-                    child: CodeInputPage(),
-                  );
-                },
-              ),
-              GoRoute(
-                path: "complete",
-                pageBuilder: (context, state) {
-                  return const MaterialPage(
-                    child: SignUpCompletePage(),
-                  );
-                },
-              ),
-            ],
-          ),
-        ],
-      )
-    ],
-  );
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -108,7 +15,7 @@ class MainApp extends StatelessWidget {
         colorSchemeSeed: Colors.pink,
         useMaterial3: true,
       ),
-      routerConfig: _router,
+      routerConfig: myRouter,
       builder: (context, child) {
         return child!;
       },
